8809ef1bd40c97ab45c782fd192f3abf80bcf865
Takeshi Akuhara <akuhara@eri.u-tokyo.ac.jp>
2021-09-18 21:59:31 +0900

    output envelope

--- src/cls_convertor.f90
+++ src/cls_convertor.f90
@@ -16,6 +16,15 @@ module cls_convertor
      integer                    :: n_sta
      integer                    :: n_cmps
      character(line_max), allocatable :: filenames(:,:,:)
+
+     ! Band pass
+     double precision :: f1 = 1.d0
+     double precision :: f2 = 3.d0
+     double precision :: f3 = 8.d0
+     double precision :: f4 = 10.d0
+     
+     ! resampling
+     integer :: n_sps = 1
      
      ! Work space for FFTW
      type(C_PTR)                            :: plan_r2c, plan_c2c, plan_c2r
@@ -29,6 +38,7 @@ module cls_convertor
 
    contains
      procedure :: convert => convertor_convert
+     procedure :: band_pass => convertor_band_pass
      procedure :: envelope => convertor_envelope
      procedure :: detrend => convertor_detrend
      procedure :: taper   => convertor_taper
@@ -70,10 +80,11 @@ contains
     integer, intent(in) :: i_sta
     double precision, allocatable :: tmp(:, :), processed(:,:)
     integer :: n2, n4, io, it, j, irow
-    logical :: first_taper
+    logical :: first_window
+    logical, parameter :: debug = .true.
     
     self%i_read_file = 1
-    self%c3(i_sta) = c3_data(files=self%filenames(1, 1:3, i_sta))
+    self%c3(i_sta) = c3_data(files=self%filenames(1, 1:self%n_cmps, i_sta))
     self%dt = self%c3(i_sta)%get_dt()
     self%n = nint(self%t_win / self%dt)
     self%c3_out(i_sta) = c3_data(dt=self%dt, n_cmps=self%n_cmps)
@@ -102,19 +113,20 @@ contains
          & FFTW_BACKWARD, FFTW_ESTIMATE)
 
     
-    tmp(1:self%n, 1:3) = 0.d0
+    tmp(1:self%n, 1:self%n_cmps) = 0.d0
     it = 0
     irow = 0
     
     write(*,*)"n_stored = ", self%c3(i_sta)%get_n_smp()
     write(*,*)self%filenames
-    first_taper = .true.
-    open(newunit=io, file="test.txt", status="replace")
+    first_window = .true.
+    if (debug) open(newunit=io, file="windows.txt", status="replace")
     do 
        if (self%c3(i_sta)%get_n_smp() < self%n) then
           self%i_read_file = self%i_read_file + 1
           write(*,*)"i_read_file=", self%i_read_file
           if (self%i_read_file > size(self%filenames(:,1,i_sta))) exit
+
           write(*,*)"enqueued from <", &
                & trim(self%filenames(self%i_read_file,1,i_sta)), " / ",&
                & trim(self%filenames(self%i_read_file,2,i_sta)), " / ",&
@@ -126,8 +138,12 @@ contains
                
           
        end if
-       tmp(1:n2,1:self%n_cmps) = tmp(n2+1:self%n,1:self%n_cmps)
-       tmp(n2+1:self%n,1:self%n_cmps) = self%c3(i_sta)%dequeue_data(n2)
+       if (.not. first_window) then
+          tmp(1:n2,1:self%n_cmps) = tmp(n2+1:self%n,1:self%n_cmps)
+          tmp(n2+1:self%n,1:self%n_cmps) = self%c3(i_sta)%dequeue_data(n2)
+       else
+          tmp(1:self%n,1:self%n_cmps) = self%c3(i_sta)%dequeue_data(self%n)
+       end if
 
        write(*,*)"dequeued: n_sotred = ", self%c3(i_sta)%get_n_smp()
 
@@ -145,41 +161,66 @@ contains
                  & self%envelope(processed(1:self%n, i_cmp))
             processed(1:self%n, i_cmp) = &
                  & self%rectangle_smoothing(processed(1:self%n, i_cmp), &
-                 & int(10.0 / self%dt))
+                 & int(2.5d0 / self%dt))
             processed(1:self%n, i_cmp) = &
                  & self%rectangle_smoothing(processed(1:self%n, i_cmp), &
-                 & int(10.0 / self%dt))
+                 & int(2.5d0 / self%dt))
          end do
        end block
        
        ! output
-       if (.not. first_taper) then
+       if (.not. first_window) then
           call self%c3_out(i_sta)%enqueue_data(&
                & processed(n4+1:self%n-n4,1:self%n_cmps))
        else
           call self%c3_out(i_sta)%enqueue_data(&
                & processed(1:self%n-n4,1:self%n_cmps))
-          first_taper = .false.
        end if
        
-       block
-         
-         double precision :: amp_fac = 4000.d0
-         do j = 1, self%n
-            write(io,*)(it + j) * self%dt, tmp(j, 1) / amp_fac + irow
-         end do
-         write(io,*)
-         do j = 1, self%n
-            write(io,*)(it + j) * self%dt, processed(j, 1) / amp_fac + irow
-         end do
-         it = it + n2
-         irow = irow + 1
-        write(io,*)
-       end block
+       if (debug)  then
+          block
+            double precision :: amp_fac = 4000.d0
+            do j = 1, self%n
+               write(io,*)(it + j) * self%dt, tmp(j, 1) / amp_fac + irow
+            end do
+            write(io,*)
+            do j = 1, self%n
+               write(io,*)(it + j) * self%dt, processed(j, 1) / amp_fac + irow
+            end do
+            it = it + n2
+            irow = irow + 1
+            write(io,*)
+          end block
+       end if
        
+       first_window = .false.
     end do
-    close(io)
+    if (debug) close(io)
     
+    ! End 
+    call self%c3_out(i_sta)%enqueue_data(&
+         & processed(self%n-n4+1:self%n,1:self%n_cmps))
+
+
+    block 
+      integer :: n_out, i, n_fac
+      double precision, allocatable :: x_out(:,:)
+      
+      open(newunit=io, file='output.txt', status='replace')
+      n_out = self%c3_out(1)%get_n_smp()
+      allocate(x_out(1:n_out,1:self%n_cmps))
+      x_out = self%c3_out(1)%get_data()
+      n_fac = nint(1.d0 / self%dt / self%n_sps)
+      do i = 1, n_out
+         if (mod(i,n_fac) == 1) then
+            write(io, *) (i-1) * self%dt, x_out(i, 1) / 4000.d0
+         end if
+      end do
+      close(io)
+
+    end block
+
+
     return 
   end subroutine convertor_convert
 
@@ -211,23 +252,57 @@ contains
   end function convertor_rectangle_smoothing
 
   !---------------------------------------------------------------------
+  
+  function convertor_band_pass(self, x) result(x_out)
+    class(convertor), intent(inout) :: self
+    complex(kind(0d0)), intent(in) :: x(self%n)
+    complex(kind(0d0)) :: x_out(self%n)
+    double precision :: df, flt
+    integer :: if1, if2, if3, if4, i
+    double precision, parameter :: pi = acos(-1.d0)
+
+    df = 1.d0 / (self%n * self%dt)
+    if1 = nint(self%f1 / df) + 1
+    if2 = nint(self%f2 / df) + 1
+    if3 = nint(self%f3 / df) + 1
+    if4 = nint(self%f4 / df) + 1
+    
+    do i = 1, self%n
+       if (i < if1) then
+          flt = 0.d0
+       else if (i < if2) then
+          flt = 0.5d0 * (1.d0 - cos((i-if1)*pi/(if2-if1)))
+       else if (i < if3) then
+          flt = 1.d0
+       else if (i < if4) then
+          flt = 0.5d0 * (1.d0 + cos((i-if3)*pi/(if4-if3)))
+       else
+          flt = 0.d0
+       end if
+       x_out(i) = x(i) * flt
+    end do
+    
+    return 
+  end function convertor_band_pass
+  
+  !---------------------------------------------------------------------
 
   function convertor_envelope(self, x) result(env)
     class(convertor), intent(inout) :: self
     double precision, intent(in) :: x(self%n)
     double precision :: env(self%n)
+    
 
 
 
     self%r_tmp(1:self%n) = x(1:self%n)
     call fftw_execute_dft_r2c(self%plan_r2c, self%r_tmp, self%c_tmp)
-    
+    self%c_tmp = self%band_pass(self%c_tmp)
     self%c_in_tmp(1) = (0.d0, 0.d0)
     self%c_in_tmp(2:self%n/2+1) = 2.d0 * self%c_tmp(2:self%n/2+1)
     self%c_in_tmp(self%n/2+2:self%n) = (0.d0, 0.d0)
     
     call fftw_execute_dft(self%plan_c2c, self%c_in_tmp, self%c_out_tmp)
-
     env(1:self%n) = abs(self%c_out_tmp(1:self%n) / self%n) 
     
     return 
